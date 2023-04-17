655f4d2d356ad6a57a2a13803a5c0e3362264351
Isack <isacksantos@uol.com.br>
2023-04-17 09:51:42 -0300

    Estrutura for(Laço de repetição

---
+++ estruturafor.py
@@ -0,0 +1,11 @@
+carros = {
+    "Polo": 80000,
+    "Gol": 70000,
+    "Corsa": 30000,
+    "Volvo XC40":205000,
+    "Audi RS3": 500000,
+    "Mustang": 420000
+}
+
+for k,v in carros.items():
+    print("O carro "+ k +" custa "+ str(v))
